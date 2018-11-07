b0864602c942bf82ace3618b92a2e65ba7604441
krystian <krystian.czekalski@resourcekraft.com>
2018-11-07 22:34:02 +0000

    Images Done

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../api.service';
+import { GlobalsService } from '../globals.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css'],
-  providers: [ApiService]
+  providers: [ApiService, GlobalsService]
 })
 export class DetailComponent implements OnInit {
 
   movie;
+  mediaUrl: string;
 
-  constructor(private route: ActivatedRoute, private api: ApiService) { }
+  constructor(private route: ActivatedRoute,
+    private api: ApiService,
+    private globals: GlobalsService) {
+      this.mediaUrl = this.globals.getBaseUrl() + '/media/';
+    }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
