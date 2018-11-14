1e9cfd66102a7c71b93307262ed3d9162ec2660c
krystian <krystian.czekalski@resourcekraft.com>
2018-11-14 23:02:06 +0000

    Different web layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
 import { ApiService } from '../api.service';
 import { GlobalsService } from '../globals.service';
+import { DetailComponent } from '../detail/detail.component';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +13,11 @@ export class HomeComponent implements OnInit {
   title = 'movies';
   movies = [];
   mediaUrl: string;
+  @ViewChild('moviecontainer', { read: ViewContainerRef }) tmpl: ViewContainerRef;
 
   constructor( private api: ApiService,
-    private globals: GlobalsService) {
+    private globals: GlobalsService,
+    private resolver: ComponentFactoryResolver) {
     this.mediaUrl = this.globals.getBaseUrl() + '/media/';
   }
 
@@ -29,6 +32,12 @@ export class HomeComponent implements OnInit {
       error => {
         console.log(error);
       }
-    )
+    );
+  }
+  movieClicked(movie) {
+    this.tmpl.clear();
+    const factory = this.resolver.resolveComponentFactory(DetailComponent);
+    const componentRef = this.tmpl.createComponent(factory);
+    componentRef.instance.idPassed = movie.id;
   }
 }
